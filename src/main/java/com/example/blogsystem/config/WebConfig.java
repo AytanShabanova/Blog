b0f0529ca77a00a8c8490b7eb698ce9ac4461b5b
AytanShabanova <you@example.com>
2023-12-15 13:30:30 -0800

    comment webconfig class

--- src/main/java/com/example/blogsystem/config/WebConfig.java
+++ src/main/java/com/example/blogsystem/config/WebConfig.java
@@ -1,27 +1,23 @@
-package com.example.blogsystem.config;
-
-import com.example.blogsystem.dto.UserDto;
-import com.example.blogsystem.mapper.UserMapper;
-import com.example.blogsystem.models.User;
-import org.slf4j.Logger;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class WebConfig {
-//@Bean
-//    UserMapper userMapper(){
-//    return new UserMapper() {
-//        @Override
-//        public UserDto toUserDto(User user) {
-//            return null;
-//        }
+//package com.example.blogsystem.config;
 //
-//        @Override
-//        public User toUserEntity(UserDto userDto) {
-//            return null;
-//        }
-
-
-
-}
+//
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class WebConfig {
+////@Bean
+////    UserMapper userMapper(){
+////    return new UserMapper() {
+////        @Override
+////        public UserDto toUserDto(User user) {
+////            return null;
+////        }
+////
+////        @Override
+////        public User toUserEntity(UserDto userDto) {
+////            return null;
+////        }
+//
+//
+//
+//}
