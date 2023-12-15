9a756c4506f01ab6faf2466e7939872f4d773882
AytanShabanova <you@example.com>
2023-12-14 22:45:13 -0800

    1

--- src/main/java/com/example/blogsystem/config/SpringDocumentConfig.java
+++ src/main/java/com/example/blogsystem/config/SpringDocumentConfig.java
@@ -1,4 +1,23 @@
 package com.example.blogsystem.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition
 public class SpringDocumentConfig {
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI().info(new Info().title("Shabanova Aytan tərəfindən Documentasiya")
+                .version("0.0.1").description("bu service Blog sayti ucun yazilmisdir")
+                .contact(new Contact()
+                        .url("www.google.com")
+                        .email("shabanovaaytan@gmail.com")
+                        .name("Shabanova Aytan")));
+    }
 }
+
